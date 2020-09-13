9edf7ba1d2c7ab7f5db02ad0ebb2f1f0fb9431c8
mk <mahendra.be18@gmail.com>
2020-09-13 22:29:33 +0530

    Add project files.

---
+++ GymOK/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GymOK.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+        public ActionResult Trainers()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Classes()
+        {
+            return View();
+        }
+        public ActionResult Nutrition()
+        {
+            return View();
+        }
+
+    }
+}
